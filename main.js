cd57f49b50e0b3aff8679b36c9443d6c58aecbfa
ProfThales <128181535+ProfThales@users.noreply.github.com>
2024-03-21 17:10:02 -0300

    time

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for(let i=0;i <botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T23:59:59");
+const tempoObjetivo1 = new Date("2024-04-19T23:59:59");
 let tempoAtual = new Date();
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
